6ab68934403e58058a87d26c25319fdeecfd26c0
Shankha <ribhumaster@gmail.com>
2022-09-17 20:43:26 +0530

    applied routes and separated logics to diff files

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end(); // shouln't change response after this. If done will cause ERROR
-});
+const routes = require("./routes");
+
+const server = http.createServer(routes);
 
 server.listen(3000);
