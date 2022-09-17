6bc25eaf97c4bc5b6fa4e1691a366cd6566329ea
Shankha <ribhumaster@gmail.com>
2022-09-17 21:01:46 +0530

    assignment done

--- routes.js
+++ routes.js
@@ -1,18 +1,23 @@
-const fs = require("fs");
-
 function requestHandler(req, res) {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
+    res.write("<head><title>Assignment</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>"
+      "<body><form action='/create-user' method='POST'><input type='text' name='username' /><button type='submit'>Create User</button></form></body>"
     );
     res.write("</html>");
     return res.end();
   }
-  if (url === "/message" && method === "POST") {
+  if (url === "/users") {
+    res.write("<html>");
+    res.write("<head><title>Assignment Users</title></head>");
+    res.write("<body><ul><li>User 1</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/create-user" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
       body.push(chunk);
@@ -20,17 +25,13 @@ function requestHandler(req, res) {
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
+      console.log(message);
+      return res.end();
     });
   }
-  res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("<head><title>Page 404</title></head>");
+  res.write("<body><h1>Page not Found!</h1></body>");
   res.write("</html>");
   res.end();
 }
